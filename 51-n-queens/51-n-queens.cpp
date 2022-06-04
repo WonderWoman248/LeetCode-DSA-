b21028ce4e6d32cf0cfe43033fac6ff0e986b927
Prashansa <67244355+WonderWoman248@users.noreply.github.com>
2022-06-05 02:06:15 +0530

    Time: 11 ms (51.87%), Space: 7.3 MB (55.07%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<string>>ans;
+    bool isSafe(vector<string>&board,int row, int col){
+        int n=size(board);
+        
+        for(int i=0;i<n;i++){
+            if(board[i][col]=='Q')return false;
+            
+            if(row-i>=0 and col-i>=0 and board[row-i][col-i]=='Q') return false;
+            if(row-i>=0 and col+i<n and board[row-i][col+i]=='Q') return false;
+            
+        }
+        
+        return true;
+    }
+    
+    void solve(vector<string>& board, int row) {
+	   if(row == size(board)) { 
+		    ans.push_back(board);
+		    return;
+    	}            
+	   
+	    for(int col = 0; col < size(board); col++){
+		    if(isSafe(board, row, col)) {
+			    board[row][col] = 'Q';    
+			    solve(board, row + 1);    
+			    board[row][col] = '.';    
+		    }
+        }
+    }
+     
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string>board(n,string(n,'.'));
+        solve(board,0);
+        return ans;
+        
+    }
+};
