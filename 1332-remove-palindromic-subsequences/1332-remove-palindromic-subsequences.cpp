2da59c2b276136d491dfd0f2bd4da9700a52aff9
Prashansa <67244355+WonderWoman248@users.noreply.github.com>
2022-06-08 19:10:07 +0530

    Time: 2 ms (43.62%), Space: 6.1 MB (78.83%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int removePalindromeSub(string s) {
-      string k=s;
-      reverse(k.begin(),k.end());
+      int i=0,j=s.size()-1;
         
-        int count=0,countA=0, countB=0;
-        
-        for(int i=0;i<s.size();i++){
-            if(s[i]!=k[i])count++;
-            if(s[i]=='a')countA++;
-            if(s[i]=='b')countB++;
+        while(i<s.size() and j>=0){
+            if(s[i]!=s[j]) return 2;
+            i++;
+            j--;
         }
-        
-        if(count==0)return 1;
-        
-        else if(countA==s.size() or countB==s.size()) return 1;
-        
-        else return 2;
+        return 1;
         
     }
 };
