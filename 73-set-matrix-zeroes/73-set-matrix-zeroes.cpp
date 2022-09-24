9ae563fcbb1c348f93dd424035c44c057a850390
Prashansa <67244355+WonderWoman248@users.noreply.github.com>
2022-09-25 03:04:32 +0530

    Time: 31 ms (29.72%), Space: 13.2 MB (81.96%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -3,17 +3,9 @@ public:
 
     void setMatrixZero(vector<vector<int>>&matrix,bool flag1, bool flag2){
     
-        for(int i=1;i<matrix[0].size();i++){
-            if(matrix[0][i]==0){
-                for(int j=1;j<matrix.size();j++){
-                    matrix[j][i]=0;
-                }
-            }
-        }
-       
         for(int i=1;i<matrix.size();i++){
-            if(matrix[i][0]==0){
-                for(int j=1;j<matrix[0].size();j++){
+            for(int j=1;j<matrix[0].size();j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
             }
