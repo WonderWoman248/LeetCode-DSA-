8c235f33d4a059c3dec143d36684b646526380bd
Prashansa <67244355+WonderWoman248@users.noreply.github.com>
2023-10-21 23:14:40 +0530

    Time: 1924 ms (11.11%), Space: 32.7 MB (22.22%) - LeetHub

---
+++ 1286-constrained-subsequence-sum/1286-constrained-subsequence-sum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def constrainedSubsetSum(self, nums, k):
+      heap = [(-nums[0], 0)]
+      ans = nums[0]
+      
+      for i in range(1, len(nums)):
+          while i - heap[0][1] > k:
+              heapq.heappop(heap)
+
+          curr = max(0, -heap[0][0]) + nums[i]
+          ans = max(ans, curr)
+          heapq.heappush(heap, (-curr, i))
+
+      return ans
+        
+        
